ac6692f8d288c0aad06523183aed7e5d7142d064
Philip Laxamana <philip.laxamana@hotmail.com>
2018-04-05 01:08:12 -0400

    added login and authentication functionality

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -9,10 +9,34 @@ export class AuthService {
 
   constructor(private http: Http) { }
 
+  // Register user service
   registerUser(user){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
     .map(res => res.json());
+  }
+
+  // Authenticate uiser service
+  authenticateUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
+    .map(res => res.json());
+  }
+
+  // Store user data
+  storeUserData(token, user){
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  // Logout
+  logout(){
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
   }
 }
